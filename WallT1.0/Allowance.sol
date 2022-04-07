a7b365503e6ef0a3fe0c5c4824aa53b87a1487bf
"moonman369" <"mightyayan369@gmail.com">
2022-04-08 01:04:09 +0530

    8.4.22

---
+++ WallT1.0/Allowance.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract Allowance is Ownable{
+
+    event allowanceChanged(address indexed _member, address indexed changer, uint _oldAmount, uint _newAmount);
+
+    mapping (address => uint) public allowance;
+
+    function setAllowance (address _member, uint _allowance) public onlyOwner{
+        emit allowanceChanged(_member, msg.sender, allowance[_member], _allowance);
+        allowance[_member] = _allowance;
+    }
+
+    modifier ownerOrAllowed (uint _amount) {
+        require(msg.sender == owner() || allowance[msg.sender] >= _amount, "You are not allowed!!");
+        _;
+    }
+
+    function reduceAllowance (address _member, uint _amount) internal {
+        emit allowanceChanged(_member, msg.sender, allowance[_member], allowance[_member]-_amount);
+        allowance[_member] -= _amount;
+    }
+}
---
+++ WallT1.0/WallT_Main.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >0.8.0;
+
+import "./Allowance.sol";
+import {Allowance as Allowance} from "./Allowance.sol";
+
+contract Wallt1 is Allowance{
+
+    event fundsDeposited(address indexed _depositor, uint _amount);
+
+    event fundsWithdrawn(address indexed _beneficiary, uint _amount);
+
+    function getWalletBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    fallback() external payable {
+        emit fundsDeposited(msg.sender, msg.value);
+    }
+
+    function withdrawFunds(address payable _to, uint _amount) public ownerOrAllowed(_amount){
+        require (_amount <= address(this).balance, "Insufficient WallT balance.");
+        if (msg.sender != owner()){
+            reduceAllowance(msg.sender, _amount);
+        }
+        emit fundsWithdrawn(_to, _amount);
+        _to.transfer(_amount);
+
+    }
+
+}
